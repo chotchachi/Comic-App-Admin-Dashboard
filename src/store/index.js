360e6ff3d275d48b72767268fa860071bb014f1b
ThanhQuang <quangtt@foxcode.org>
2021-01-02 23:50:10 +0700

    remove modules

--- src/store/index.js
+++ src/store/index.js
@@ -13,7 +13,6 @@ import state from './state'
 import getters from './getters'
 import mutations from './mutations'
 import actions from './actions'
-import modules from './modules'
 import plugins from './plugins'
 
 Vue.use(Vuex)
@@ -23,7 +22,7 @@ const strict = process.env.NODE_ENV !== 'production'
 /**
  * @type {import('vuex/types').Store<typeof import('./state').default>}
  */
-const store = new Vuex.Store({ state, getters, mutations, actions, modules, plugins, strict })
+const store = new Vuex.Store({ state, getters, mutations, actions, plugins, strict })
 
 export default store
 
@@ -32,24 +31,12 @@ if (module.hot) {
   module.hot.accept([
     './getters',
     './mutations',
-    './actions',
-    './modules/comments',
-    './modules/options',
-    './modules/posts',
-    './modules/terms',
-    './modules/users'
+    './actions'
   ], () => {
     store.hotUpdate({
       getters: require('./getters'),
       mutations: require('./mutations'),
-      actions: require('./actions'),
-      modules: {
-        comments: require('./modules/comments'),
-        options: require('./modules/options'),
-        posts: require('./modules/posts'),
-        terms: require('./modules/terms'),
-        users: require('./modules/users')
-      }
+      actions: require('./actions')
     })
   })
 }
